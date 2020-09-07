cae13f6e546f24c5dabcc983c5a35eac9b0e51b1
Aaron Lin <aaronlin256@gmail.com>
2020-09-07 16:34:17 -0400

    feat(menu): layout and samples rendered on menu

--- keyboard-layout-editor/src/app/app.module.ts
+++ keyboard-layout-editor/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
